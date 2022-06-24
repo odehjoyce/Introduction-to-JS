dbd26034181004a9a6a703b9afa4f1e5b21806ec
Odeh Joyce <odehjoyce02@gmail.com>
2022-06-24 15:53:50 -0700

    Added my project

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//Code to display name, country and height on the screen.
+
+var name = window.prompt("Enter your name: ");
+var height = window.prompt("Enter your height: ");
+var country = window.prompt("Enter your country: ");
+
+document.write("My name is "+name+". I am from "+country+". I am "+height+" feet tall");
